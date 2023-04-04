289f57cc0b771dc472085185ee5dea5d91f1200e
Brian Reardon <brian.d.reardon@gmail.com>
2023-04-04 13:07:58 -0400

    done

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,23 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
+    return num1+num2
     pass
 
 def halve(number):
+    return number/2
+
     pass
